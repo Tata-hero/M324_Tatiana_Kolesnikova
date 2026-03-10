d551b4c86f5abf26a8656d8da9d759bb3d668888
Tatiana Kolesnikova <tkolesnikova@online.gibz.ch>
2026-03-10 14:33:28 +0100

    fix: reorder variable declarations in sum function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export const sum = (a, b) => {
-        const unusedVariable = 123;
+  const name = "Tatiana";
+  const unusedVariable = 123;
   return a + b + 1 + unusedVariable;
 };
 console.log("Hello");
